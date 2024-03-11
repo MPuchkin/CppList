03e2f2ae04608007dd3bb55f3581b73eed839f53
MPuchkin <mpuchkin@mail.ru>
2024-03-11 19:13:22 +0300

    Update my_list.h
    Исправление ошибок

--- List/my_list.h
+++ List/my_list.h
@@ -1,8 +1,9 @@
 #include <initializer_list>
 #include <memory>
 #include <iterator>
+#include <stdexcept>
 
-template<typename T>
+template<typename T, typename Allocator = std::allocator<T>>
 class MyList
 {
 private:
@@ -10,23 +11,34 @@ private:
         T data;
         mynode* next;
         mynode* prev;
-        mynode(T d, mynode* p, mynode* n) : data(d), prev(p), next(n) {}
+        mynode(const T& d, mynode* p, mynode* n) : data(d), prev(p), next(n) {}
+        mynode(T&& d, mynode* p, mynode* n) : data(std::move(d)), prev(p), next(n) {}
     };
 
-    mynode* head, * tail;
+    mynode* dummy;
     size_t _size;
-public:
 
+    using AllocType = typename std::allocator_traits<Allocator>::template rebind_alloc<mynode>;
+
+    AllocType Alc;
+public:
+    using allocator_type = Allocator;
+    using pointer = typename std::allocator_traits<Allocator>::pointer;
+    using const_pointer = typename std::allocator_traits<Allocator>::const_pointer;
     using value_type = T;
     using size_type = size_t;
     using difference_type = ptrdiff_t;
-    using pointer = value_type*;
-    using const_pointer = const value_type*;
     using reference = value_type&;
     using const_reference = const value_type&;
 
+    class const_iterator;
+
     class iterator {
+        friend class MyList;
+        friend class const_iterator;
         mynode* p;
+        
+        iterator(mynode* pp) : p(pp) {}
     public:
         using value_type = T;
         using iterator_category = std::bidirectional_iterator_tag;
@@ -34,7 +46,7 @@ public:
         using pointer = T*;
         using reference = value_type&;
 
-        iterator(mynode* pp) : p(pp) {}
+        
         T& operator*() { return p->data; }
         iterator& operator++() {
             p = p->next;
@@ -45,25 +57,38 @@ public:
             p = p->next;
             return rez;
         }
-        friend bool operator==(iterator v1, iterator v2) {
+        iterator& operator--() {
+            p = p->prev;
+            return *this;
+        }
+        iterator operator--(int) {
+            iterator rez(*this);
+            p = p->prev;
+            return rez;
+        }
+        friend bool operator==(const iterator &v1, const iterator &v2) {
             return v1.p == v2.p;
         }
-        friend bool operator!=(iterator v1, iterator v2) {
+        friend bool operator!=(const iterator &v1, const iterator &v2) {
             return !(v1 == v2);
         }
     };
 
     class const_iterator {
+        
+        friend class MyList;
+
         mynode* p;
+        const_iterator(mynode* pp) : p(pp) {}
     public:
         using value_type = T;
         using iterator_category = std::bidirectional_iterator_tag;
         using difference_type = ptrdiff_t;
         using pointer = const T*;
         using reference = const value_type&;
-
-        const_iterator(mynode* pp) : p(pp) {}
+        
         const T& operator*() const { return p->data; }
+        const_iterator(iterator& it) : p(it.p) {}
         const_iterator& operator++() {
             p = p->next;
             return *this;
@@ -72,22 +97,82 @@ public:
             const_iterator rez(*this);
             p = p->next;
             return rez;
+        }        
+        const_iterator& operator--() {
+            p = p->prev;
+            return *this;
         }
-        friend bool operator==(const_iterator v1, const_iterator v2) {
+        const_iterator operator--(int) {
+            const_iterator rez(*this);
+            p = p->prev;
+            return rez;
+        }
+    private:
+        iterator get_iterator() {
+            return iterator(p);
+        }
+    public:
+
+
+        friend bool operator==(const const_iterator &v1, const const_iterator &v2) {
             return v1.p == v2.p;
         }
-        friend bool operator!=(const_iterator v1, const_iterator v2) {
+        friend bool operator!=(const const_iterator &v1, const const_iterator &v2) {
             return !(v1 == v2);
         }
     };
 
-    MyList() : head(nullptr), tail(nullptr), _size(0) {};
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+private:
+
+
+
+    inline mynode* construct_dummy() {
+        //  Выделение памяти без инициализации
+        mynode* nodeptr{ Alc.allocate(1) };
+
+        //  Инициализация полей кроме поля data - зацикливаем на себя
+        std::allocator_traits<AllocType>::construct(Alc, &(nodeptr->prev), nodeptr);
+        std::allocator_traits<AllocType>::construct(Alc, &(nodeptr->next), nodeptr);
+        return nodeptr;
+    }
+
+    inline mynode* construct_node(value_type&& elem, mynode* prev, mynode* next) {
+        //  Выделение памяти без инициализации
+        mynode* nodeptr{ Alc.allocate(1) };
+        //  Инициализация полей включая поле data
+        //  Инициализация полей кроме поля data - зацикливаем на себя
+        std::allocator_traits<AllocType>::construct(Alc, &(nodeptr->prev), prev);
+        std::allocator_traits<AllocType>::construct(Alc, &(nodeptr->next), next);
+        
+        std::allocator_traits<AllocType>::construct(Alc, &(nodeptr->data), std::move(elem));
+        return nodeptr;
+    }
+
+    inline mynode* construct_node(const value_type & elem, mynode *prev, mynode *next) {
+        return construct_node(std::move(value_type(elem)), prev, next);
+    }
+
+    inline void delete_dummy(mynode * node) {
+        std::allocator_traits<AllocType>::destroy(Alc, &(node->prev));
+        std::allocator_traits<AllocType>::destroy(Alc, &(node->next));
+        std::allocator_traits<AllocType>::deallocate(Alc, node, 1);
+    }
+
+    inline void delete_node(mynode* node) {
+        std::allocator_traits<AllocType>::destroy(Alc, &(node->data));
+        delete_dummy(node);
+    }
+
+public:
+    MyList() : dummy(construct_dummy()), _size(0) {};
     
     MyList(const MyList& other) : MyList(other.cbegin(), other.cend()) {};
     
-    MyList(MyList&& other) : head(other.head), tail(other.tail), _size(other._size) {
-        other.head = nullptr;
-        other.tail = nullptr;
+    MyList(MyList&& other) : dummy(other.dummy), _size(other._size) {
+        other.dummy = nullptr;
+        std::swap(Alc, other.Alc);
         other._size = 0;
     };
 
@@ -101,30 +186,89 @@ public:
         while (start != fin)
             push_back(*start++);
     }
-
-
-    void push_back(const T& value) {
-        mynode *newNode = new mynode(value, tail, nullptr);
-        if (tail != nullptr) {
-            tail->next = newNode;
-            tail = newNode;
-        }
-        else {
-            head = tail = newNode;
-        }
-        _size++;
+    
+    allocator_type get_allocator() const noexcept {
+        return Alc;
     }
     
-    void pop_back() {
-        if (tail == nullptr) return;
-        mynode* tmp = tail;
-        tail = tail->prev;
-        if (tail != nullptr)
-            tail->next = nullptr;
-        else
-            head = nullptr;
-        delete tmp;
+    reference front() { 
+        if(empty()) throw new std::out_of_range("Trying to pop on empty list!");
+        return dummy->next->data; 
+    }
+    
+    const_reference front() const {
+        if (empty()) throw new std::out_of_range("Trying to pop on empty list!");
+        return dummy->next->data;
+    };
+
+    reference back() {
+        if (empty()) throw new std::out_of_range("Trying to pop on empty list!");
+        return dummy->prev->data;
+    };
+
+    const_reference back() const {
+        if (empty()) throw new std::out_of_range("Trying to pop on empty list!");
+        return dummy->prev->data;
+    };
+
+    iterator erase(const_iterator pos) {
+        ++pos;
+        remove_node(pos.p->prev);
+        return pos.get_iterator();
+    }
+
+    iterator erase(const_iterator first, const_iterator last) {
+        mynode* left = first.p->prev;
+        mynode* tmp = first.p;
+        while (tmp != last.p) {
+            tmp = tmp->next;
+            remove_node(tmp->prev);
+        }
+        left->next = last.p;
+        last.p->prev = left;
+
+        return last.get_iterator();
+    }
+
+    void push_back(T&& value) {
+        mynode* newNode = new mynode(std::move(value), dummy->prev, dummy);
+        dummy->prev->next = newNode;
+        dummy->prev = newNode;
+        _size++;
+    }
+
+    void push_back(const T& value) {
+        push_back(std::move(T(value)));
+    }
+
+private:
+    void remove_node(mynode* node) {
+        if (node == dummy)
+            throw new std::out_of_range("Trying to pop on empty list!");
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        delete_node(node);
         _size--;
+    }
+
+public:
+    void pop_back() {
+        remove_node(dummy->prev);
+    }
+
+    void push_front(T&& value) {
+        mynode* newNode = new mynode(std::move(value), dummy, dummy->next);
+        dummy->next->prev = newNode;
+        dummy->next = newNode;
+        _size++;
+    }
+
+    void push_front(const T& value) {
+        push_front(std::move(T(value)));
+    }
+
+    void pop_front() {
+        remove_node(dummy->next);
     }
 
     bool empty() const noexcept { return _size == 0; }
@@ -138,27 +282,56 @@ public:
             pop_back();
     };
 
-    ~MyList() { clear(); }
+    ~MyList() {
+        if (dummy != nullptr) {
+            clear();
+            delete_dummy(dummy);
+        }
+    }
 
     iterator begin() {
-        return iterator(head);
+        return iterator(dummy->next);
     }
     
-    const_iterator begin() const {
-        return const_iterator(head);
+    const iterator begin() const {
+        return const_iterator(dummy->next);
     }
 
     const_iterator cbegin() {
-        return const_iterator(head);
+        return const_iterator(dummy->next);
     }
 
     iterator end() {
-        return iterator(nullptr);
+        return iterator(dummy);
+    }
+
+    const_iterator end() const {
+        return iterator(dummy);
     }
 
     const_iterator cend() {
-        return const_iterator(nullptr);
+        return const_iterator(dummy);
     }
+    
+    reverse_iterator rbegin() noexcept {
+        return reverse_iterator(end());
+    }
+    const_reverse_iterator rbegin() const noexcept {
+        return const_reverse_iterator(cend());
+    }
+    const_reverse_iterator crbegin() const noexcept {
+        return const_reverse_iterator(cend());
+    }
+    reverse_iterator rend() noexcept {
+        return reverse_iterator(begin());
+    }
+    const_reverse_iterator rend() const noexcept {
+        return const_reverse_iterator(cbegin());
+    }
+    const_reverse_iterator crend() const noexcept {
+        return const_reverse_iterator(cbegin());
+    }
+
 };
 
 
